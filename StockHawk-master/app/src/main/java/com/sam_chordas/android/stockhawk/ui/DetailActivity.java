8c8d105a2fec689dadb00beba82918ade239c3a8
fl0r1d4 <lian@mail.usf.edu>
2017-01-05 11:42:41 -0500

    Edittext listener added

--- StockHawk-master/app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailActivity.java
+++ StockHawk-master/app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.sam_chordas.android.stockhawk.R;
 import com.sam_chordas.android.stockhawk.data.QuoteColumns;
@@ -23,14 +26,21 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_detail);
+
+        EditText text = (EditText) findViewById(R.id.title_editText);
+        text.setOnKeyListener(onSoftKeyboardDonePress);
+
+
+
         Intent recieved = getIntent();
         if(recieved != null) {
-            String symbol = recieved.getStringExtra("Stock_id");
+           /* String symbol = recieved.getStringExtra("Stock_id");
              Cursor c = getContentResolver().query(StockContract.StockEntry.CONTENT_URI,
                     new String[] { QuoteColumns.CHANGE, QuoteColumns.BIDPRICE }, QuoteColumns.SYMBOL + "= ?",
                     new String[] { symbol }, null);
-            nextChartData(symbol,true);
+            nextChartData(symbol,true);*/
         }
 
 
@@ -41,7 +51,7 @@ public class DetailActivity extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                nextChartData("",true);
             }
         });
     }
@@ -54,4 +64,16 @@ public class DetailActivity extends AppCompatActivity {
         }
         chartView.setPoints(pricePoints);
     }
+    private View.OnKeyListener onSoftKeyboardDonePress=new View.OnKeyListener()
+    {
+        public boolean onKey(View v, int keyCode, KeyEvent event)
+        {
+            if (event.getKeyCode() == KeyEvent.KEYCODE_ENTER || event.getKeyCode() == KeyEvent.KEYCODE_1)
+            {
+                Toast.makeText(DetailActivity.this, "checking event", Toast.LENGTH_LONG).show();
+            }
+            return false;
+        }
+    };
+
 }
