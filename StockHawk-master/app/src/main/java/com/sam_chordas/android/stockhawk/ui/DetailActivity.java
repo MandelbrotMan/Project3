6d9efe67fe183720b2966041fb0feb4046a608f7
fl0r1d4 <lian@mail.usf.edu>
2017-01-20 21:21:16 -0500

    new lineview library added to the app

--- StockHawk-master/app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailActivity.java
+++ StockHawk-master/app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailActivity.java
@@ -2,12 +2,22 @@ package com.sam_chordas.android.stockhawk.ui;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.ChartTouchListener;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.sam_chordas.android.stockhawk.R;
 import com.sam_chordas.android.stockhawk.data.QuoteColumns;
 import com.sam_chordas.android.stockhawk.data.StockContract;
@@ -23,11 +33,14 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity  extends DemoBase implements SeekBar.OnSeekBarChangeListener,
+        OnChartGestureListener, OnChartValueSelectedListener {
     private static final double MAX_Y = 100;
-    private LineChartView chartView;
-    List<LineChartView.Point> pricePoints = new ArrayList<>();
-    List<LineChartView.Point> changePoints = new ArrayList<>();
+    private LineChart mChartView;
+    private SeekBar mBarSetFrequency, mBarRangeScale;
+    private TextView mFrequencyText, mDateRangeText;
+    List<PointF> pricePoints = new ArrayList<>();
+    List<PointF> changePoints = new ArrayList<>();
     List<Integer> months = new ArrayList<>(); //Every entry will contain the #value of the month
 
     String[] mProjection = {StockContract.StockEntry.COLUMN_BIDPRICE, StockContract.StockEntry.COLUMN_CHANGE,
@@ -49,18 +62,30 @@ public class DetailActivity extends AppCompatActivity {
             createPriceGraph(mCursor);
 
 
-            chartView = (LineChartView) findViewById(R.id.chart_view);
-            chartView.setManualMinY(0);
+            mChartView = (LineChart) findViewById(R.id.chart_view);
+            mBarSetFrequency = (SeekBar) findViewById(R.id.seekBar1);
+            mBarRangeScale = (SeekBar) findViewById(R.id.seekBar2);
+            mDateRangeText = (TextView) findViewById(R.id.textViewDateRange);
+            mFrequencyText = (TextView) findViewById(R.id.textViewFrequency);
+
+            mBarRangeScale.setMax(4);
+            mBarRangeScale.setProgress(1);
+            mBarRangeScale.setMax(60);
+            mBarSetFrequency.setProgress(6);
+
+            mChartView.setOnChartGestureListener(this);
+            mChartView.setOnChartValueSelectedListener(this);
+            mChartView.setDrawGridBackground(false);
+
+
+
+
 
             View nextButton = findViewById(R.id.next_data);
             nextButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    List<LineChartView.Point> NewPricePoints = new ArrayList<>();
-                    for(int i = 0; i < 5; ++i){
-                        NewPricePoints.add(pricePoints.get(i));
-                    }
-                    chartView.setPoints(NewPricePoints);
+
 
                 }
             });
@@ -71,7 +96,7 @@ public class DetailActivity extends AppCompatActivity {
 
         for (int i = 0; i < 5; i++) {
             int y = (int) (Math.random() * MAX_Y);
-            pricePoints.add(new LineChartView.Point(i, y));
+            pricePoints.add(new PointF(i, y));
         }
 //        chartView.setPoints(pricePoints);
     }
@@ -83,9 +108,11 @@ public class DetailActivity extends AppCompatActivity {
                 mProjection, StockContract.StockEntry.COLUMN_STOCK_SYMBOL + "= ?",
                 new String[]{mSymbol}, null);
 
+        float min = -1;
+
         if(mCursor.moveToFirst()){
             while(mCursor.moveToNext()){
-                double numberValue;
+                float numberValue;
                 String price = mCursor.getString(1);
                 String dateValue = mCursor.getString(3);// 1 is the array location for the column of prices
                 Log.v("Date", dateValue);
@@ -93,8 +120,10 @@ public class DetailActivity extends AppCompatActivity {
                 by not entering it to the array
                  */
                 try {
-                     numberValue = Double.parseDouble(price);
-
+                     numberValue = Float.parseFloat(price);
+                    if(min > numberValue){
+                        min = numberValue - 1;
+                    }
                      Date date = DateFormat.getDateTimeInstance().parse(dateValue);
 
                      Calendar cal = GregorianCalendar.getInstance();
@@ -103,7 +132,7 @@ public class DetailActivity extends AppCompatActivity {
                      int minute = cal.get(Calendar.MINUTE);
                     Log.v("minute", Integer.toString(minute));
                      months.add(cal.get(Calendar.MONTH));
-                   pricePoints.add(new LineChartView.Point(minute, (long) numberValue));
+                   pricePoints.add(new PointF(minute, numberValue));
                 }catch (NumberFormatException e){
                     Log.v("Number Exception", "Confirmed");
                 } catch (ParseException e) {
@@ -113,7 +142,78 @@ public class DetailActivity extends AppCompatActivity {
             }
             mCursor.close();
         }
+        mChartView.setMinimumHeight(-10);
     }
 
 
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onChartGestureStart(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+        Log.i("Gesture", "START, x: " + me.getX() + ", y: " + me.getY());
+    }
+
+    @Override
+    public void onChartGestureEnd(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+        Log.i("Gesture", "END, lastGesture: " + lastPerformedGesture);
+
+        // un-highlight values after the gesture is finished and no single-tap
+        if(lastPerformedGesture != ChartTouchListener.ChartGesture.SINGLE_TAP)
+            mChartView.highlightValues(null); // or highlightTouch(null) for callback to onNothingSelected(...)
+    }
+
+    @Override
+    public void onChartLongPressed(MotionEvent me) {
+        Log.i("LongPress", "Chart longpressed.");
+    }
+
+    @Override
+    public void onChartDoubleTapped(MotionEvent me) {
+        Log.i("DoubleTap", "Chart double-tapped.");
+    }
+
+    @Override
+    public void onChartSingleTapped(MotionEvent me) {
+        Log.i("SingleTap", "Chart single-tapped.");
+    }
+
+    @Override
+    public void onChartFling(MotionEvent me1, MotionEvent me2, float velocityX, float velocityY) {
+        Log.i("Fling", "Chart flinged. VeloX: " + velocityX + ", VeloY: " + velocityY);
+    }
+
+    @Override
+    public void onChartScale(MotionEvent me, float scaleX, float scaleY) {
+        Log.i("Scale / Zoom", "ScaleX: " + scaleX + ", ScaleY: " + scaleY);
+    }
+
+    @Override
+    public void onChartTranslate(MotionEvent me, float dX, float dY) {
+        Log.i("Translate / Move", "dX: " + dX + ", dY: " + dY);
+    }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+        Log.i("Entry selected", e.toString());
+        Log.i("LOWHIGH", "low: " + mChartView.getLowestVisibleX() + ", high: " + mChartView.getHighestVisibleX());
+        Log.i("MIN MAX", "xmin: " + mChartView.getXChartMin() + ", xmax: " + mChartView.getXChartMax() + ", ymin: " + mChartView.getYChartMin() + ", ymax: " + mChartView.getYChartMax());
+    }
+
+    @Override
+    public void onNothingSelected() {
+        Log.i("Nothing selected", "Nothing selected.");
+    }
 }
